54c705e65903ce4d4acbb0292bd000192ef148bb
Alex Zierov <alexzierov1@gmail.com>
2021-12-17 00:22:11 +0200

    homework_9 renamed correctly and almost done

---
+++ homework_9/task9.1.py
@@ -0,0 +1,26 @@
+# Написать функцию которая принимает на вход параметры : список чисел любой
+#    дины и число .
+#    Функция должна проверить есть ли в списке 2 числа сума которых
+#    еквивалентна числу переданому 2м параметром. Функция должна вернуть
+#    булевое значение - результат поиска фукции.
+#    Для проверки вызвать 2 раза функцию с разными тестовыми примерами.
+
+import random
+
+
+def operation(a, x):
+    o = 1
+    for i in range(0, len(a)):
+        for j in range(o, len(a)):
+            if a[i] + a[j] == x:
+                return True
+        o += 1
+    return False
+
+
+my_list = [random.randint(1, 10) for y in range(10)]
+print(my_list)
+my_integer = random.randint(2, 20)
+print(my_integer)
+print('')
+print(operation(my_list, my_integer))
