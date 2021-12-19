5a438dc7abde074aff8f60959f669e469c2221a4
Alex Zierov <alexzierov1@gmail.com>
2021-12-19 19:34:16 +0200

    homework 9 is fixed

--- homework_9/task9.3.py
+++ homework_9/task9.3.py
@@ -2,12 +2,46 @@
 # чисел. Вывести в консоль результат работы функции-генератора  в диапазоне
 # 1,100 в одну строчку через пробел
 
-def prime_numbers_generator(x):
-    for i in range(2, x):
-        if x % i == 0:
-            return False
-    return True
+# def prime_numbers_generator(x):
+#     if x == 1:
+#         return False
+#     else:
+#         for i in range(2, x):
+#             if x % i == 0:
+#                 return False
+#         return True
+#
+#
+# prime = ([x for x in range(1, 100) if prime_numbers_generator(x)])
+# print(*prime)
+#
+# from sympy import isprime
+#
+#
+# def prime_numbers(n):
+#     for prime in range(n):
+#         if isprime(i):
+#             yield i
+#
+#
+# a = prime_numbers(100)
+#
+# for i in a:
+#     print(i, end=" ")
 
 
-prime = ([x for x in range(1, 100) if prime_numbers_generator(x)])
-print(*prime)
+def prime_numbers(n):
+    o = 2
+    for i in range(2, n):
+        for j in range(2, o):
+            if i % j == 0:
+                break
+        else:
+            yield i
+        o += 1
+
+
+a = prime_numbers(100)
+
+for i in a:
+    print(i, end=" ")
